a88535d5996376fb1c5a5ec1a9c79279d8919710
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-04-08 19:47:03 +0600

    firebase auth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import './Style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    <form class="box" action="index.html" method="post">
+      <h1>Login</h1>
+      <input type="text" name="" placeholder="Username" />
+      <input type="password" name="" placeholder="Password" />
+      <input type="submit" name="" value="Login" />
+      <p class="social-text">Or Sign up with social platforms</p>
+      <div class="social-media">
+        <button class="social-icon">
+          <i class="fab fa-google"></i>
+        </button>
+        <button class="social-icon">
+          <i class="fab fa-facebook-f"></i>
+        </button>
+        <button class="social-icon">
+          <i class="fab fa-twitter"></i>
+        </button>
+        <button class="social-icon">
+          <i class="fab fa-linkedin-in"></i>
+        </button>
+      </div>
+      <div class="link">
+        <a href="#">Forgot password?</a> or <a href="#">Sign Up</a>
+      </div>
+    </form>
+    // </div>
   );
 }
 

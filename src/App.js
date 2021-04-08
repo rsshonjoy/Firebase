ffdd7bba935795300d6c1375035cf94ba739da59
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-04-08 20:43:38 +0600

    google auth

--- src/App.js
+++ src/App.js
@@ -1,34 +1,56 @@
-import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import { useForm } from "react-hook-form";
+import firebaseConfig from "./firebaseConfig";
 import './Style.css';
 
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+}else {
+  firebase.app();
+}
+
 function App() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+  
+  const { handleSubmit } = useForm();
+  const onSubmit = data => console.log(data);
+
+  const handleSignIn = () => {
+    firebase.auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      console.log(result);
+      
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
   return (
-    // <div className="App">
-    <form class="box" action="index.html" method="post">
+    <form className="box" action="index.html" method="post" onSubmit={handleSubmit(onSubmit)}>
       <h1>Login</h1>
       <input type="text" name="" placeholder="Username" />
       <input type="password" name="" placeholder="Password" />
       <input type="submit" name="" value="Login" />
-      <p class="social-text">Or Sign up with social platforms</p>
-      <div class="social-media">
-        <button class="social-icon">
-          <i class="fab fa-google"></i>
+      <p className="social-text">Or Sign up with social platforms</p>
+      <div className="social-media">
+        <button className="social-icon" onClick={handleSignIn}>
+          <i className="fab fa-google"></i>
         </button>
-        <button class="social-icon">
-          <i class="fab fa-facebook-f"></i>
+        <button className="social-icon" onClick={handleSignIn}>
+          <i className="fab fa-facebook-f"></i>
         </button>
-        <button class="social-icon">
-          <i class="fab fa-twitter"></i>
+        <button className="social-icon">
+          <i className="fab fa-twitter"></i>
         </button>
-        <button class="social-icon">
-          <i class="fab fa-linkedin-in"></i>
+        <button className="social-icon">
+          <i className="fab fa-linkedin-in"></i>
         </button>
       </div>
-      <div class="link">
+      <div className="link">
         <a href="#">Forgot password?</a> or <a href="#">Sign Up</a>
       </div>
     </form>
-    // </div>
   );
 }
 

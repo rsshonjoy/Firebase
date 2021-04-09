41b62a7e11192082251ccf0c4a154a2a7c9152af
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-04-09 08:57:45 +0600

    form field validation using regular expression

--- src/App.js
+++ src/App.js
@@ -20,13 +20,29 @@ function App() {
     email: '',
     photo: ''
   })
-
-  const provider = new firebase.auth.GoogleAuthProvider();
   
   const { handleSubmit } = useForm();
   const onSubmit = data => console.log(data);
 
-  const handleSignIn = () => {
+
+  // email and password auth
+  const handleBlur = (e) => {
+    console.log(e.target.value);
+    if (e.target.name === 'email') {
+      const isEmailValid = /\S+@\S+\.\S+/.test(e.target.value)
+      console.log("🚀 ~ file: App.js ~ line 30 ~ handleBlur ~ isEmailValid", isEmailValid)
+    }
+    if (e.target.name === 'password') {
+      const isPasswordValid = e.target.value.length > 5;
+      const passwordHasValid = /\d{1}/.test(e.target.value);
+      console.log("🚀 ~ file: App.js ~ line 35 ~ handleBlur ~ isPasswordValid", isPasswordValid && passwordHasValid)
+    }
+  }
+
+  
+  // google auth
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handleGoogleSignIn = () => {
     firebase.auth()
     .signInWithPopup(provider)
     .then((result) => {
@@ -61,8 +77,7 @@ function App() {
     <div className="App">
 
       {
-        user.isSignIn ? 
-          <button onClick={handleSignOut}>Sign Out</button>: <button onClick={handleSignIn}>Sign In</button>
+        user.isSignIn ? <button onClick={handleSignOut}>Sign Out</button> : <button onClick={handleGoogleSignIn}>Sign In</button>
       }
 
       {
@@ -74,17 +89,19 @@ function App() {
         </div>
       }
       <br/><br/>
+
+      {/* Login form */}
       <form className="box" action="index.html" method="post" onSubmit={handleSubmit(onSubmit)}>
           <h1>Login</h1>
-          <input type="text" name="" placeholder="Username" />
-          <input type="password" name="" placeholder="Password" />
+          <input type="text" name="email" onBlur={handleBlur} placeholder="Username" required />
+          <input type="password" name="password" onBlur={handleBlur} placeholder="Password" required />
           <input type="submit" name="" value="Login" />
           <p className="social-text">Or Sign up with social platforms</p>
           <div className="social-media">
-            <button className="social-icon" onClick={handleSignIn}>
+            <button className="social-icon" onClick={handleGoogleSignIn}>
               <i className="fab fa-google"></i>
             </button>
-            <button className="social-icon" onClick={handleSignIn}>
+            <button className="social-icon">
               <i className="fab fa-facebook-f"></i>
             </button>
             <button className="social-icon">
